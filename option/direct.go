3854606f5439a0985508ad286312f14b3040dfaf
starifly <2003879+starifly@users.noreply.github.com>
2025-01-11 22:36:04 +0800

    增加 TLS 分片

--- option/direct.go
+++ option/direct.go
@@ -12,4 +12,10 @@ type DirectOutboundOptions struct {
 	OverrideAddress string `json:"override_address,omitempty"`
 	OverridePort    uint16 `json:"override_port,omitempty"`
 	ProxyProtocol   uint8  `json:"proxy_protocol,omitempty"`
+  Fragment        *Fragment `json:"fragment,omitempty"`
+}
+
+type Fragment struct {
+	Length   string `json:"length"`
+	Interval string `json:"interval"`
 }
