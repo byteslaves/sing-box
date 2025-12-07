efdc5dbcf1e2575ce0fbfeec611a5b7578db7039
starifly <2003879+starifly@users.noreply.github.com>
2025-12-07 11:35:50 +0800

    xhttp improvements

--- transport/v2rayxhttp/dialer.go
+++ transport/v2rayxhttp/dialer.go
@@ -78,6 +78,9 @@ func (c *DefaultDialerClient) OpenStream(ctx context.Context, url string, body i
 			return
 		}
 		if resp.StatusCode != 200 || uploadOnly { // stream-up
+			if resp.StatusCode != 200 {
+				c.closed = true
+			}
 			io.Copy(io.Discard, resp.Body)
 			resp.Body.Close() // if it is called immediately, the upload will be interrupted also
 			wrc.Close()
@@ -105,6 +108,7 @@ func (c *DefaultDialerClient) PostPacket(ctx context.Context, url string, body i
 		_, copyErr := io.Copy(io.Discard, resp.Body)
 		closeErr := resp.Body.Close()
 		if resp.StatusCode != 200 {
+			c.closed = true
 			if copyErr != nil {
 				return copyErr
 			}
@@ -152,6 +156,7 @@ func (c *DefaultDialerClient) PostPacket(ctx context.Context, url string, body i
 					_, copyErr := io.Copy(io.Discard, resp.Body)
 					closeErr := resp.Body.Close()
 					if resp.StatusCode != 200 {
+						c.closed = true
 						return fmt.Errorf("got non-200 error response code: %d", resp.StatusCode)
 					}
 					if copyErr != nil {
