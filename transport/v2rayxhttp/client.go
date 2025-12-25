f513f7b7badab55e5ac63c3973479a1a039ed70a
starifly <2003879+starifly@users.noreply.github.com>
2025-12-25 21:51:28 +0800

    xhttp improvements

--- transport/v2rayxhttp/client.go
+++ transport/v2rayxhttp/client.go
@@ -172,7 +172,11 @@ func (c *Client) DialContext(ctx context.Context) (net.Conn, error) {
 	requestURL := c.getRequestURL(sessionIdUuid.String())
 	requestURL2 := c.getRequestURL2(sessionIdUuid.String())
 	httpClient, xmuxClient := c.getHTTPClient()
-	httpClient2, xmuxClient2 := c.getHTTPClient2()
+	var httpClient2 DialerClient
+	var xmuxClient2 *XmuxClient
+	if mode != "stream-one" || c.downloadDest != nil {
+		httpClient2, xmuxClient2 = c.getHTTPClient2()
+	}
 	httpVersion := httpVersionFromClient(httpClient)
 	destLabel := formatDestWithNetwork(httpClient, c.dest)
 	logger := c.logger
