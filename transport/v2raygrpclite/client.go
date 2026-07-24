1343656e19a1ba0818b2dc18409de5b64bf58f30
byteslaves <40004076+byteslaves@users.noreply.github.com>
2026-07-24 21:57:36 +0800

    Change User-Agent in gRPC client header
    Signed-off-by: byteslaves <40004076+byteslaves@users.noreply.github.com>

--- transport/v2raygrpclite/client.go
+++ transport/v2raygrpclite/client.go
@@ -23,7 +23,7 @@ var _ adapter.V2RayClientTransport = (*Client)(nil)
 
 var defaultClientHeader = http.Header{
 	"Content-Type": []string{"application/grpc"},
-	"User-Agent":   []string{"grpc-go/1.48.0"},
+	"User-Agent":   []string{"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"},
 	"TE":           []string{"trailers"},
 }
 
